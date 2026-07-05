2014077aeaf2766ecfbb67ae649cb668aa72e986
Wei <lhwei.winni@gmail.com>
2026-07-05 18:02:20 +0800

    fix 路徑

--- lib/nutrition/WeightProjectionCard.tsx
+++ lib/nutrition/WeightProjectionCard.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useEffect, useState } from 'react'
-import LoadingSpinner from './LoadingSpinner'
+import LoadingSpinner from '../../components/LoadingSpinner'
 
 interface ProjectionData {
   currentWeight: number
